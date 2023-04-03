53d753a167f35e1afc5b2db6f1506cd4b7365388
lizhicheng <Aa123456>
2023-04-03 15:08:15 +0800

    优化25

--- src/main/java/com/orange/demo/DemoApplication.java
+++ src/main/java/com/orange/demo/DemoApplication.java
@@ -3,10 +3,14 @@ package com.orange.demo;
 import com.orange.demo.view.PrimaryStageView;
 import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class DemoApplication extends AbstractJavaFxApplicationSupport{
@@ -22,7 +26,16 @@ public class DemoApplication extends AbstractJavaFxApplicationSupport{
         stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
             public void handle(WindowEvent event) {
-                System.exit(0);
+                //增加退出提示框
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.titleProperty().set("退出");
+                alert.headerTextProperty().set("确定要退出吗？");
+                Optional<ButtonType> result = alert.showAndWait();
+                if(result.get() == ButtonType.OK){
+                    System.exit(0);
+                }else{
+                    event.consume();
+                }
             }
         });
     }
